58daa7d71412cf79a4799c6d1c89a36b252bae15
hault <hault.cm@gmail.com>
2022-06-19 12:52:15 +0700

    lan1

---
+++ app/src/main/java/org/o7planning/android2dgame/GameSurface.java
@@ -0,0 +1,144 @@
+package org.o7planning.android2dgame;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
+    private GameThread gameThread;
+
+    private final List<ChibiCharactor> chibiList = new ArrayList<ChibiCharactor>();
+    private final List<Explosion> explosionList = new ArrayList<Explosion>();
+
+    public GameSurface(Context context)  {
+        super(context);
+
+        // Make Game Surface focusable so it can handle events.
+        this.setFocusable(true);
+
+        // Sét callback.
+        this.getHolder().addCallback(this);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            int x=  (int)event.getX();
+            int y = (int)event.getY();
+
+            Iterator<ChibiCharactor> iterator= this.chibiList.iterator();
+
+            while(iterator.hasNext()) {
+                ChibiCharactor chibi = iterator.next();
+                if( chibi.getX() < x && x < chibi.getX() + chibi.getWidth()
+                        && chibi.getY() < y && y < chibi.getY()+ chibi.getHeight())  {
+                    // Remove the current element from the iterator and the list.
+                    iterator.remove();
+
+                    // Create Explosion object.
+                    Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.explosion);
+                    Explosion explosion = new Explosion(this, bitmap,chibi.getX(),chibi.getY());
+
+                    this.explosionList.add(explosion);
+                }
+            }
+
+
+            for(ChibiCharactor chibi: chibiList) {
+                int movingVectorX =x-  chibi.getX() ;
+                int movingVectorY =y-  chibi.getY() ;
+                chibi.setMovingVector(movingVectorX, movingVectorY);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public void update()  {
+        for(ChibiCharactor chibi: chibiList) {
+            chibi.update();
+        }
+        for(Explosion explosion: this.explosionList)  {
+            explosion.update();
+        }
+
+        Iterator<Explosion> iterator= this.explosionList.iterator();
+        while(iterator.hasNext())  {
+            Explosion explosion = iterator.next();
+
+            if(explosion.isFinish()) {
+                // If explosion finish, Remove the current element from the iterator & list.
+                iterator.remove();
+                continue;
+            }
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas)  {
+        super.draw(canvas);
+
+        for(ChibiCharactor chibi: chibiList)  {
+            chibi.draw(canvas);
+        }
+
+        for(Explosion explosion: this.explosionList)  {
+            explosion.draw(canvas);
+        }
+
+    }
+
+    // Implements method of SurfaceHolder.Callback
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Bitmap chibiBitmap1 = BitmapFactory.decodeResource(this.getResources(),R.drawable.chibi1);
+        ChibiCharactor chibi1 = new ChibiCharactor(this,chibiBitmap1,100,50);
+
+        Bitmap chibiBitmap2 = BitmapFactory.decodeResource(this.getResources(),R.drawable.chibi2);
+        ChibiCharactor chibi2 = new ChibiCharactor(this,chibiBitmap2,300,150);
+
+        this.chibiList.add(chibi1);
+        this.chibiList.add(chibi2);
+
+        this.gameThread = new GameThread(this,holder);
+        this.gameThread.setRunning(true);
+        this.gameThread.start();
+    }
+
+    // Implements method of SurfaceHolder.Callback
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    // Implements method of SurfaceHolder.Callback
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry= true;
+        while(retry) {
+            try {
+                this.gameThread.setRunning(false);
+
+                // Parent thread must wait until the end of GameThread.
+                this.gameThread.join();
+            }catch(InterruptedException e)  {
+                e.printStackTrace();
+            }
+            retry= true;
+        }
+    }
+}
---
+++ app/src/main/java/org/o7planning/android2dgame/MainActivity.java
@@ -0,0 +1,24 @@
+package org.o7planning.android2dgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setContentView(new GameSurface(this));
+    }
+}
