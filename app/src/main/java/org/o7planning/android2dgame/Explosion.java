673352ad1823da52a01e2fea96921c6a9d4d33f8
hault <hault.cm@gmail.com>
2022-06-19 13:08:13 +0700

    last

--- app/src/main/java/org/o7planning/android2dgame/Explosion.java
+++ app/src/main/java/org/o7planning/android2dgame/Explosion.java
@@ -17,6 +17,11 @@ public class Explosion extends GameObject{
     public void update()  {
         this.colIndex++;
 
+        // Play sound explosion.wav.
+        if(this.colIndex==0 && this.rowIndex==0) {
+            this.gameSurface.playSoundExplosion();
+        }
+
         if(this.colIndex >= this.colCount)  {
             this.colIndex =0;
             this.rowIndex++;
@@ -37,4 +42,5 @@ public class Explosion extends GameObject{
     public boolean isFinish() {
         return finish;
     }
+
 }
--- app/src/main/java/org/o7planning/android2dgame/GameSurface.java
+++ app/src/main/java/org/o7planning/android2dgame/GameSurface.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -19,6 +23,15 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
     private final List<ChibiCharacter> chibiList = new ArrayList<ChibiCharacter>();
     private final List<Explosion> explosionList = new ArrayList<Explosion>();
 
+    private static final int MAX_STREAMS=100;
+    private int soundIdExplosion;
+    private int soundIdBackground;
+
+    private boolean soundPoolLoaded;
+    private SoundPool soundPool;
+
+
+
     public GameSurface(Context context)  {
         super(context);
 
@@ -27,8 +40,67 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
 
         // Sét callback.
         this.getHolder().addCallback(this);
+
+        this.initSoundPool();
     }
 
+    private void initSoundPool()  {
+        // With Android API >= 21.
+        if (Build.VERSION.SDK_INT >= 21 ) {
+
+            AudioAttributes audioAttrib = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            SoundPool.Builder builder= new SoundPool.Builder();
+            builder.setAudioAttributes(audioAttrib).setMaxStreams(MAX_STREAMS);
+
+            this.soundPool = builder.build();
+        }
+        // With Android API < 21
+        else {
+            // SoundPool(int maxStreams, int streamType, int srcQuality)
+            this.soundPool = new SoundPool(MAX_STREAMS, AudioManager.STREAM_MUSIC, 0);
+        }
+
+        // When SoundPool load complete.
+        this.soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                soundPoolLoaded = true;
+
+                // Playing background sound.
+                playSoundBackground();
+            }
+        });
+
+        // Load the sound background.mp3 into SoundPool
+        this.soundIdBackground= this.soundPool.load(this.getContext(), R.raw.background,1);
+
+        // Load the sound explosion.wav into SoundPool
+        this.soundIdExplosion = this.soundPool.load(this.getContext(), R.raw.explosion,1);
+
+
+    }
+
+    public void playSoundExplosion()  {
+        if(this.soundPoolLoaded) {
+            float leftVolumn = 0.8f;
+            float rightVolumn =  0.8f;
+            // Play sound explosion.wav
+            int streamId = this.soundPool.play(this.soundIdExplosion,leftVolumn, rightVolumn, 1, 0, 1f);
+        }
+    }
+
+    public void playSoundBackground()  {
+        if(this.soundPoolLoaded) {
+            float leftVolumn = 0.8f;
+            float rightVolumn =  0.8f;
+            // Play sound background.mp3
+            int streamId = this.soundPool.play(this.soundIdBackground,leftVolumn, rightVolumn, 1, -1, 1f);
+        }
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -38,6 +110,7 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
             int y = (int)event.getY();
 
             Iterator<ChibiCharacter> iterator= this.chibiList.iterator();
+
 
             while(iterator.hasNext()) {
                 ChibiCharacter chibi = iterator.next();
@@ -138,4 +211,5 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
             retry= true;
         }
     }
+
 }
